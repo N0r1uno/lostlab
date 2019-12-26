10de67a101dcb5e6c0e30e18d1738e9f64e32b69
Steffen Florschuetz <ge93fir@mytum.de>
2019-12-26 14:27:46 +0100

    improved actor controller

--- twinlab-unity/Assets/Scripts/Actor.cs
+++ twinlab-unity/Assets/Scripts/Actor.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Actor : MonoBehaviour
 {
     public enum MovementDirection
@@ -13,52 +15,76 @@ public class Actor : MonoBehaviour
     [Header("Actor Stats")]
     public MovementDirection direction;
     public float speed;
+    public float inAirSpeed;
     public float jumpForce;
     public InputAction input;
 
     private new Rigidbody2D rigidbody;
+    private new Collider2D collider;
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         input = new InputAction();
     }
 
     public void ApplyMovement()
     {
-        //rigidbody.velocity.Set(input.horizontalInput * speed, jumpForce);
-        rigidbody.velocity = new Vector2(input.horizontalInput * speed, input.isJumping?jumpForce:0);
+        if (IsGrounded())
+        {
+            if (input.isJumping)
+                rigidbody.AddForce(transform.up * jumpForce);
+            rigidbody.velocity = new Vector2(input.horizontalInput * speed, rigidbody.velocity.y);
+        } else
+            rigidbody.velocity = new Vector2(Mathf.Clamp(rigidbody.velocity.x + input.horizontalInput * inAirSpeed, -speed, speed), rigidbody.velocity.y);
+
+        //direction
         direction = input.direction;
+        spriteRenderer.flipX = direction == MovementDirection.LEFT;
     }
 
     virtual public bool IsPlayer => false;
 
+    public bool IsGrounded()
+    {
+        RaycastHit2D[] result = new RaycastHit2D[1];
+        collider.Raycast(Vector2.down, result);
+        if (result[0].collider == null)
+            return false;
+        return collider.IsTouching(result[0].collider);
+    }
+
     public class InputAction
     {
         public float horizontalInput;
-        public bool isJumping, isCrouching;
+        public bool isJumping, isCrouching, isSprinting;
         public MovementDirection direction;
 
         public InputAction()
         {
-            this.horizontalInput = 0f;
-            this.isJumping = false;
-            this.isCrouching = false;
+            horizontalInput = 0f;
+            isJumping = false;
+            isCrouching = false;
+            isSprinting = false;
         }
 
-        public void Set(float horizontalInput, bool isJumping, bool isCroutching)
+        public void Set(float horizontalInput, bool isJumping, bool isCrouching, bool isSprinting)
         {
             this.horizontalInput = horizontalInput;
             this.isJumping = isJumping;
-            this.isCrouching = isCroutching;
+            this.isCrouching = isCrouching;
+            this.isSprinting = isSprinting;
             direction = (horizontalInput == 0) ? direction : (horizontalInput > 0) ? MovementDirection.RIGHT : MovementDirection.LEFT;
         }
 
         public void Get()
         {
-            Set(Input.GetAxis("Horizontal"), Input.GetKeyDown(KeyCode.Space), Input.GetKey(KeyCode.LeftControl));
+            Set(Input.GetAxis("Horizontal"), Input.GetKeyDown(KeyCode.Space), Input.GetKey(KeyCode.LeftControl), Input.GetKey(KeyCode.LeftShift));
         }
 
-        override
+        override //TODO
         public string ToString()
         {
             return "";
