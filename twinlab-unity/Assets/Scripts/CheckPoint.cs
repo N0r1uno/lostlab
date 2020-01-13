b7372af979bc5a87d20bac96050abc23479589b3
Steffen Florschuetz <ge93fir@mytum.de>
2020-01-13 16:43:55 +0100

    Added Terminal Prefab

--- twinlab-unity/Assets/Scripts/CheckPoint.cs
+++ twinlab-unity/Assets/Scripts/CheckPoint.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CheckPoint : Interactable
 {
-    
+
+    [Header("Check Point Specific")]
+    public Sprite deactivatedCheckpoint;
+    public Sprite activatedCheckpoint;
+    public Light screenLight;
+
+    private bool active;
+    private new SpriteRenderer renderer;
     void Start()
     {
+        renderer = GetComponent<SpriteRenderer>();
+        active = false;
         ShowMessage(false);
+        renderer.sprite = deactivatedCheckpoint;
+        screenLight.enabled = false;
     }
 
     public override void Interact()
     {
         CheckPointManager.SetCheckPoint(this);
+        if (!active)
+        {
+            active = true;
+            renderer.sprite = activatedCheckpoint;
+            screenLight.enabled = true;
+        }
+            
     }
 
     override public void ShowMessage(bool val)
