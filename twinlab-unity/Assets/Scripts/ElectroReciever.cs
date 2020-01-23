af681b8fd5f4c3fca062b9d869d05c9f23b07088
ge35mit <ge35mit@mytum.de>
2020-01-23 10:39:25 +0100

    no message

---
+++ twinlab-unity/Assets/Scripts/ElectroReciever.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectroReciever : MonoBehaviour
+{
+    public Sprite on;
+    public Sprite off;
+
+    public UnityEngine.Events.UnityEvent eventOn;
+
+    public bool isOn;
+
+    public void turnOn()
+    {
+        if (!isOn)
+        {
+            isOn = true;
+            GetComponent<SpriteRenderer>().sprite = on;
+            eventOn.Invoke();
+        }
+    }
+}
