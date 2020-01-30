7c99c69ad954d859a6030b96e1b9444a586e19f5
Steffen Florschuetz <ge93fir@mytum.de>
2020-01-30 11:03:41 +0100

    Cables & PowerPotions work now

---
+++ twinlab-unity/Assets/Scripts/Cable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Cable : MonoBehaviour
+{
+    public Door door;
+    public Sprite on;
+    public Sprite off;
+    public float timeout = 2f;
+
+    private bool isOn = false;
+    private new SpriteRenderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void GiveSignal()
+    {
+        if (!isOn)
+        {
+            door.Open();
+            renderer.sprite = on;
+            StartCoroutine(TurnOff());
+        }
+    }
+
+    IEnumerator TurnOff()
+    {
+        isOn = true;
+        yield return new WaitForSeconds(timeout);
+        isOn = false;
+        renderer.sprite = off;
+        door.Close();
+    }
+}
